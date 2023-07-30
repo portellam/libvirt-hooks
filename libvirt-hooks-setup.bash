4fbea555d6c6704642df7615a92ae42c8f52ecaa
portellam <amportell.13@gmail.com>
2023-07-30 14:32:45 -0400

    Reorganized and compartmentalized logic for each file type. Disabled binary file logic for now.

--- libvirt-hooks-setup.bash
+++ libvirt-hooks-setup.bash
@@ -2,7 +2,7 @@
 
 #
 # Filename:       libvirt-hooks-setup.bash
-# Description:    Manages libvirt-hooks binaries and files.
+# Description:    Manages libvirt-hooks binaries, scripts and services.
 # Author(s):      Alex Portell <github.com/portellam>
 # Maintainer(s):  Alex Portell <github.com/portellam>
 #
@@ -28,6 +28,36 @@
     readonly PREFIX_ERROR="${SET_COLOR_YELLOW}An error occurred:${RESET_COLOR}"
     readonly PREFIX_FAIL="${SET_COLOR_RED}Failure:${RESET_COLOR}"
     readonly PREFIX_PASS="${SET_COLOR_GREEN}Success:${RESET_COLOR}"
+
+    readonly BIN_DEST_PATH="/usr/local/bin/libvirt-hooks/"
+    readonly BIN_SOURCE_PATH="bin"
+    readonly SERVICE_DEST_PATH="/etc/systemd/system/"
+    readonly SERVICE_SOURCE_PATH="systemd"
+    readonly HOOK_DEST_PATH="/etc/libvirt/hooks/"
+    readonly HOOK_SOURCE_PATH="hooks"
+
+    declare -ar BIN_LIST=(
+      "libvirt-dohibernate"
+      "libvirt-dosleep"
+    )
+
+    declare -ar HOOK_LIST=(
+      "cfscpu"
+      "ddcutil"
+      # "dohibernate"
+      # "dosleep"
+      "hugepages"
+      "isolcpu"
+      "nosleep"
+      "qemu"
+      "set-hooks"
+    )
+
+    declare -ar SERVICE_LIST=(
+      # "libvirt-dohibernate@.service"
+      # "libvirt-dosleep@.service"
+      "libvirt-nosleep@.service"
+    )
 # </params>
 
 # <functions>
@@ -57,213 +87,75 @@
 
     GetOption || exit 1
 
-    local -r BIN_DEST_PATH="/usr/local/bin/libvirt-hooks/"
-    local -r BIN_SOURCE_PATH="bin"
-    local -r SERVICE_DEST_PATH="/etc/systemd/system/"
-    local -r SERVICE_SOURCE_PATH="systemd"
-    local -r HOOK_DEST_PATH="/etc/libvirt/hooks/"
-    local -r HOOK_SOURCE_PATH="hooks"
-
-    local -ar BIN_LIST=(
-      "libvirt-dohibernate"
-      "libvirt-dosleep"
-    )
-
-    local -ar HOOK_LIST=(
-      "cfscpu"
-      "ddcutil"
-      # "dohibernate"
-      # "dosleep"
-      "hugepages"
-      "isolcpu"
-      "nosleep"
-      "qemu"
-      "set-hooks"
-    )
-
-    local -ar SERVICE_LIST=(
-      # "libvirt-dohibernate@.service"
-      # "libvirt-dosleep@.service"
-      "libvirt-nosleep@.service"
-    )
-
-    if "${DO_INSTALL}"; then
-      if ! Install; then
-        echo -e "${PREFIX_FAIL} Could not install ${REPO_NAME}."
-        exit 1
-      else
-        echo -e "${PREFIX_PASS} Installed ${REPO_NAME}."
-      fi
-    else
-      if ! Uninstall; then
-        echo -e "${PREFIX_FAIL} Could not uninstall ${REPO_NAME}."
-        exit 1
-      else
-        echo -e "${PREFIX_PASS} Uninstalled ${REPO_NAME}."
-      fi
-
+    if ! "${DO_INSTALL}"; then
+      Uninstall
+      exit "${?}"
     fi
 
-    exit 0
+    Install
+    exit "${?}"
   }
 
-  function DeleteDestinationBinaries
-  {
-    if [[ ! -d "${BIN_DEST_PATH}" ]]; then
+  # <summary>Copy Source Files to Destination</summary>
+    function CopySourceFilesToDestination
+    {
+      # CopyBinaryFilesToDestination || return 1
+      CopyScriptFilesToDestination || return 1
+      CopyServiceFilesToDestination || return 1
       return 0
-    fi
+    }
 
-    cd "${BIN_DEST_PATH}"
+    function CopyBinaryFilesToDestination
+    {
+      cd ..
+      cd "${BIN_SOURCE_PATH}" || return 1
 
-    for BIN in "${BIN_LIST[@]}"; do
-      if ! rm -rf "${BIN}" &> /dev/null; then
-        echo -e "${PREFIX_ERROR} Failed to delete project binaries."
+      if ! sudo cp --recursive --force * "${BIN_DEST_PATH}" &> /dev/null; then
+        echo -e "${PREFIX_ERROR} Failed to copy project binaries."
         return 1
       fi
-    done
 
-    return 0
-  }
-
-  function DeleteDestinationScripts
-  {
-    if [[ ! -d "${HOOK_DEST_PATH}" ]]; then
       return 0
-    fi
+    }
 
-    cd "${HOOK_DEST_PATH}"
+    function CopyScriptFilesToDestination
+    {
+      cd ..
+      cd "${HOOK_SOURCE_PATH}" || return 1
 
-    for HOOK in "${HOOK_LIST[@]}"; do
-      if ! rm -rf "${HOOK}" &> /dev/null; then
-        echo -e "${PREFIX_ERROR} Failed to delete project script(s)."
+      if ! sudo cp --recursive --force * "${HOOK_DEST_PATH}" &> /dev/null; then
+        echo -e "${PREFIX_ERROR} Failed to copy project script(s)."
         return 1
       fi
-    done
 
-    return 0
-  }
-
-  function DeleteDestinationServices
-  {
-    if [[ ! -d "${SERVICE_DEST_PATH}" ]]; then
       return 0
-    fi
+    }
 
-    cd "${SERVICE_DEST_PATH}"
+    function CopyServiceFilesToDestination
+    {
+      cd .. || return 1
+      cd "${SERVICE_SOURCE_PATH}" || return 1
 
-    for SERVICE in "${SERVICE_LIST[@]}"; do
-      if ! rm -rf "${SERVICE}" &> /dev/null; then
-        echo -e "${PREFIX_ERROR} Failed to delete project service(s)."
+      if ! sudo cp --recursive --force * "${SERVICE_DEST_PATH}" &> /dev/null; then
+        echo -e "${PREFIX_ERROR} Failed to copy project service(s)."
         return 1
       fi
-    done
 
-    return 0
-  }
-
-  function DoBinariesExist
-  {
-    cd "${WORKING_DIR}"
-    cd "${BIN_SOURCE_PATH}" || return 1
-
-    for BIN in "${BIN_LIST[@]}"; do
-      if [[ ! -e "${BIN}" ]]; then
-        echo -e "${PREFIX_ERROR} Missing project binaries."
-        return 1
-      fi
-    done
-
-    return 0
-  }
-
-  function DoScriptsExist
-  {
-    cd "${WORKING_DIR}"
-    cd "${HOOK_SOURCE_PATH}" || return 1
-
-    for HOOK in "${HOOK_LIST[@]}"; do
-      if [[ ! -e "${HOOK}" ]]; then
-        echo -e "${PREFIX_ERROR} Missing project scripts."
-        return 1
-      fi
-    done
-
-    return 0
-  }
-
-  function DoServicesExist
-  {
-    cd "${WORKING_DIR}"
-    cd "${SERVICE_SOURCE_PATH}" || return 1
-
-    for SERVICE in "${SERVICE_LIST[@]}"; do
-      if [[ ! -e "${SERVICE}" ]]; then
-        echo -e "${PREFIX_ERROR} Missing project services."
-        return 1
-      fi
-    done
-
-    return 0
-  }
-
-  function DoesDestinationPathExist
-  {
-    # if [[ ! -d "${BIN_DEST_PATH}" ]] \
-    #   && ! sudo mkdir -p "${BIN_DEST_PATH}"; then
-    #   echo -e "${PREFIX_ERROR} Could not create directory '${BIN_DEST_PATH}'."
-    #   return 1
-    # fi
-
-    if [[ ! -d "${HOOK_DEST_PATH}" ]]; then
-      echo -e "${PREFIX_ERROR} Could not find directory '${HOOK_DEST_PATH}'."
-      return 1
-    fi
-
-    if [[ ! -d "${SERVICE_DEST_PATH}" ]]; then
-      echo -e "${PREFIX_ERROR} Could not find directory '${SERVICE_DEST_PATH}'."
-      return 1
-    fi
-
-    return 0
-  }
-
-  function CopyFilesToDesination
-  {
-    # cd ..
-    # cd "${BIN_SOURCE_PATH}" || return 1
-
-    # if ! sudo cp -rf * "${BIN_DEST_PATH}" &> /dev/null; then
-    #   echo -e "${PREFIX_ERROR} Failed to copy project binaries."
-    #   return 1
-    # fi
-
-    cd ..
-    cd "${HOOK_SOURCE_PATH}" || return 1
-
-    if ! sudo cp -rf * "${HOOK_DEST_PATH}" &> /dev/null; then
-      echo -e "${PREFIX_ERROR} Failed to copy project script(s)."
-      return 1
-    fi
-
-    cd .. || return 1
-    cd "${SERVICE_SOURCE_PATH}" || return 1
-
-    if ! sudo cp -rf * "${SERVICE_DEST_PATH}" &> /dev/null; then
-      echo -e "${PREFIX_ERROR} Failed to copy project service(s)."
-      return 1
-    fi
-
-    return 0
-  }
+      return 0
+    }
 
   function Install
   {
-    # DoBinariesExist || return 1
-    DoScriptsExist || return 1
-    DoServicesExist || return 1
-    DoesDestinationPathExist || return 1
-    CopyFilesToDesination || return 1
-    SetFilePermissions || return 1
+    if ! DoSourceFilesExist \
+      || ! DoesDestinationPathExist \
+      || ! CopySourceFilesToDestination \
+      || ! SetPermissionsForDestinationFiles; then
+      echo -e "${PREFIX_FAIL} Could not install ${REPO_NAME}."
+      return 1
+    fi
+
+    echo -e "${PREFIX_PASS} Installed ${REPO_NAME}."
+    return 0
   }
 
   function PrintUsage
@@ -283,30 +175,207 @@
     return 0
   }
 
-  function SetFilePermissions
-  {
-    # if ! sudo chown -R root:root "${BIN_DEST_PATH}" &> /dev/null \
-    #   || ! sudo chmod -R +x "${BIN_DEST_PATH}" &> /dev/null \
-    #   || ! sudo chmod -R +x "${HOOK_DEST_PATH}" &> /dev/null \
-    #   || ! sudo chown -R root:root "${SERVICE_DEST_PATH}" &> /dev/null; then
-    #   echo -e "${PREFIX_ERROR} Failed to set file permissions."
-    #   return 1
-    # fi
+  # <summary>Delete Destination Files</summary>
+    function DeleteDestinationFiles
+    {
+      DeleteBinaryFiles || return 1
+      DeleteScriptFiles || return 1
+      DeleteServiceFiles || return 1
+    }
 
-    if ! sudo chmod -R +x "${HOOK_DEST_PATH}" &> /dev/null \
-      || ! sudo chown -R root:root "${SERVICE_DEST_PATH}" &> /dev/null; then
-      echo -e "${PREFIX_ERROR} Failed to set file permissions."
-      return 1
-    fi
+    function DeleteBinaryFiles
+    {
+      if [[ ! -d "${BIN_DEST_PATH}" ]]; then
+        return 0
+      fi
 
-    return 0
-  }
+      cd "${BIN_DEST_PATH}"
+
+      for BIN in "${BIN_LIST[@]}"; do
+        if ! rm --recursive --force "${BIN}" &> /dev/null; then
+          echo -e "${PREFIX_ERROR} Failed to delete project binaries."
+          return 1
+        fi
+      done
+
+      return 0
+    }
+
+    function DeleteScriptFiles
+    {
+      if [[ ! -d "${HOOK_DEST_PATH}" ]]; then
+        return 0
+      fi
+
+      cd "${HOOK_DEST_PATH}"
+
+      for HOOK in "${HOOK_LIST[@]}"; do
+        if ! rm --recursive --force "${HOOK}" &> /dev/null; then
+          echo -e "${PREFIX_ERROR} Failed to delete project script(s)."
+          return 1
+        fi
+      done
+
+      return 0
+    }
+
+    function DeleteServiceFiles
+    {
+      if [[ ! -d "${SERVICE_DEST_PATH}" ]]; then
+        return 0
+      fi
+
+      cd "${SERVICE_DEST_PATH}"
+
+      for SERVICE in "${SERVICE_LIST[@]}"; do
+        if ! rm --recursive --force "${SERVICE}" &> /dev/null; then
+          echo -e "${PREFIX_ERROR} Failed to delete project service(s)."
+          return 1
+        fi
+      done
+
+      return 0
+    }
+
+  # <summary>Do Source Files Exist</summary>
+    function DoSourceFilesExist
+    {
+      # DoBinaryFilesExist || return 1
+      DoScriptFilesExist || return 1
+      DoServiceFilesExist || return 1
+    }
+
+    function DoBinaryFilesExist
+    {
+      cd "${WORKING_DIR}"
+      cd "${BIN_SOURCE_PATH}" || return 1
+
+      for BIN in "${BIN_LIST[@]}"; do
+        if [[ ! -e "${BIN}" ]]; then
+          echo -e "${PREFIX_ERROR} Missing project binaries."
+          return 1
+        fi
+      done
+
+      return 0
+    }
+
+    function DoScriptFilesExist
+    {
+      cd "${WORKING_DIR}"
+      cd "${HOOK_SOURCE_PATH}" || return 1
+
+      for HOOK in "${HOOK_LIST[@]}"; do
+        if [[ ! -e "${HOOK}" ]]; then
+          echo -e "${PREFIX_ERROR} Missing project scripts."
+          return 1
+        fi
+      done
+
+      return 0
+    }
+
+    function DoServiceFilesExist
+    {
+      cd "${WORKING_DIR}"
+      cd "${SERVICE_SOURCE_PATH}" || return 1
+
+      for SERVICE in "${SERVICE_LIST[@]}"; do
+        if [[ ! -e "${SERVICE}" ]]; then
+          echo -e "${PREFIX_ERROR} Missing project services."
+          return 1
+        fi
+      done
+
+      return 0
+    }
+
+  # <summary>Do Destination Paths Exist</summary>
+    function DoesDestinationPathExist
+    {
+      # DoesBinaryPathExist || return 1
+      DoesScriptPathExist || return 1
+      DoesServicePathExist || return 1
+      return 0
+    }
+
+    function DoesBinaryPathExist
+    {
+      if [[ ! -d "${BIN_DEST_PATH}" ]] \
+        && ! sudo mkdir --parents "${BIN_DEST_PATH}" &> /dev/null; then
+        echo -e "${PREFIX_ERROR} Could not create directory '${BIN_DEST_PATH}'."
+        return 1
+      fi
+
+      return 0
+    }
+
+    function DoesScriptPathExist
+    {
+      if [[ ! -d "${HOOK_DEST_PATH}" ]]; then
+        echo -e "${PREFIX_ERROR} Could not find directory '${HOOK_DEST_PATH}'."
+        return 1
+      fi
+      return 0
+    }
+
+    function DoesServicePathExist
+    {
+      if [[ ! -d "${SERVICE_DEST_PATH}" ]]; then
+        echo -e "${PREFIX_ERROR} Could not find directory '${SERVICE_DEST_PATH}'."
+        return 1
+      fi
+
+      return 0
+    }
+
+  # <summary>Set Permissions For Destination Files</summary>
+    function SetPermissionsForDestinationFiles
+    {
+      # SetPermissionsForBinaryFiles || return 1
+      SetPermissionsForScriptFiles || return 1
+      SetPermissionsForServiceFiles || return 1
+    }
+
+    function SetPermissionsForBinaryFiles
+    {
+      if ! sudo chown --recursive --silent root:root "${BIN_DEST_PATH}" \
+        || ! sudo chmod --recursive --silent +x "${BIN_DEST_PATH}"; then
+        echo -e "${PREFIX_ERROR} Failed to set file permissions for binaries."
+        return 1
+      fi
+
+      return 0
+    }
+
+    function SetPermissionsForScriptFiles
+    {
+      if ! sudo chown --recursive --silent root:root "${HOOK_DEST_PATH}"; then
+        echo -e "${PREFIX_ERROR} Failed to set file permissions for script(s)."
+        return 1
+      fi
+
+      return 0
+    }
+
+    function SetPermissionsForServiceFiles
+    {
+      if ! sudo chown --recursive --silent root:root "${SERVICE_DEST_PATH}"; then
+        echo -e "${PREFIX_ERROR} Failed to set file permissions for service(s)."
+        return 1
+      fi
+
+      return 0
+    }
 
   function Uninstall
   {
-    # DeleteDestinationBinaries || return 1
-    DeleteDestinationScripts || return 1
-    DeleteDestinationServices || return 1
+    if ! DeleteDestinationFiles; then
+      echo -e "${PREFIX_FAIL} Could not uninstall ${REPO_NAME}."
+      return 1
+    fi
+
+    echo -e "${PREFIX_PASS} Uninstalled ${REPO_NAME}."
     return 0
   }
 # </functions>
